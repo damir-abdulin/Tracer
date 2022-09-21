4f55f0369ce009e32ebb2598ffad565eef1c92e2
Damir Abdulin <damir.abdulin@tutanota.com>
2022-09-21 13:25:32 +0300

    [feature] Add classes for writing results

---
+++ Core/Serialization/Writer/FileWriter.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Core.Serialization.Writer
+{
+    internal class FileWriter : IResultWriter
+    {
+        private string _destination;
+        public FileWriter(string destination)
+        {
+            _destination = destination;
+        }
+
+        void IResultWriter.Write(string result)
+        {
+            using (StreamWriter writer = new StreamWriter(_destination, false))
+            {
+                writer.WriteLine(result);
+            }
+        }
+    }
+}
