55d97b4bde5816e5e487499312f75e44a001a75e
Damir Abdulin <damir.abdulin@tutanota.com>
2022-09-21 11:58:42 +0300

    [feature] Add example

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -8,6 +8,30 @@ namespace Examples
         static private ITracer _tracer = new Tracer();
         static void Main(string[] args)
         {
+            _tracer.StartTrace();
+
+            InnerMethod1();
+            InnerMethod2();
+
+            _tracer.StopTrace();
+
+            var result = _tracer.GetTraceResult();
+        }
+
+        static void InnerMethod1()
+        {
+            _tracer.StartTrace();
+
+            InnerMethod2();
+
+            _tracer.StopTrace();
+        }
+
+        static void InnerMethod2()
+        {
+            _tracer.StartTrace();
+
+            _tracer.StopTrace();
         }
     }
 }
